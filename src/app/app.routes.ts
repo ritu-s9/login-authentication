e95b5edf8acbe6c8f613a097eab69c61979407dc
@ritu.btkol <ritu.btkol@gmail.com>
2024-08-12 11:23:44 +0530

    initial update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'auth',
+        pathMatch: 'full'
+    },
+    {
+        path:'auth',
+        loadChildren:()=> import('../app/components/auth/auth.routes').then((r) => r.routes)
+    }
+];
